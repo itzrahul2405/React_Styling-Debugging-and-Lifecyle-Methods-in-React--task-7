e3e141ca8767eb4a852ee7e606d869fcb8bd2f1d
RahulSharma <rahulmlc2003@gmail.com>
2023-11-09 16:37:04 +0530

    this is task 11

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,7 +1,20 @@
-import React from "react";
-import classes from './Input.module.css'
+import React, { useRef, useImperativeHandle } from "react";
+import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+        focus: activate
+    }
+  });
+//  useImperativeHandle  => takes 2 parameters 1. we will get in our component function argument list , most of time we will work with props      2. second parameter is a function that should return an object and that object will contain all the data we will be able to use outside ( translation object between internal functionalities and the outside world. )
+
   return (
     <div
       className={`${classes.control} ${
@@ -10,6 +23,7 @@ const Input = (props) => {
     >
       <label htmlFor={props.id}>{props.label}</label>
       <input
+        ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
@@ -18,6 +32,6 @@ const Input = (props) => {
       />
     </div>
   );
-};
+});
 
 export default Input;
